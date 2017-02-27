80b6733fa5f5dbff97afbcf6bee85dfdfcae92cd
alxl <mildwindyu@hotmail.com>
2017-02-27 11:25:45 +0800

    cleaned the model Record, fixed some logic bugs.

--- ts18/fight/admin.py
+++ ts18/fight/admin.py
@@ -4,8 +4,7 @@ class PlayerAdmin(admin.ModelAdmin):
     list_display = ('player','score','ai')
 
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('AI1','AI2','time','result','AI1_scorechange','AI2_scorechange','log')
+    list_display = ('AI1','AI2','time', 'scorechange','log')
 
 admin.site.register(models.Player,PlayerAdmin)
 admin.site.register(models.Record,RecordAdmin)
-# Register your models here.
